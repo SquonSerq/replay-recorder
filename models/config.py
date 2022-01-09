46246c0fd4d3b47f1e7566585fc654e1d296de05
SquonSerq <squonserq@bk.ru>
2022-01-09 21:19:56 +0600

    add init config settings

--- models/config.py
+++ models/config.py
@@ -33,6 +33,24 @@ class Config:
 		with open('./danser/settings/default.json', 'r') as f:
 			self.danser_config = load(f)
 
+			if not self.__is_danser_config_loaded:
+				self.danser_config['Skin']['Cursor']['UseSkinCursor'] = True
+				self.danser_config['Skin']['Cursor']['Scale'] = 0.5
+				self.danser_config['Skin']['UseColorsFromSkin'] = True
+				self.danser_config['Cursor']['Colors']['EnableRainbow'] = False
+				self.danser_config['Cursor']['CursorRipples'] = False
+				self.danser_config['Cursor']['CursorSize']  = 12
+				self.danser_config['Objects']['Sliders']['Snaking']['Out'] = False
+				self.danser_config['Objects']['Colors']['Color']['EnableRainbow'] = False
+				self.danser_config['Objects']['Colors']['UseComboColors'] = True
+				self.danser_config['Objects']['Colors']['UseSkinComboColors'] = True
+				self.danser_config['Playfield']['SeizureWarning']['Enabled'] = False
+				self.danser_config['Gameplay']['Boundaries']['Enabled'] = False
+				self.danser_config['Playfield']['Background']['Parallax']['Amount'] = 0
+				self.danser_config['Playfield']['Background']['LoadStoryboards'] = False
+				self.save_config()
+				self.__is_danser_config_loaded = True
+
 	def save_config(self):
 		with open('./danser/settings/default.json', 'w') as f:
 			dump(self.danser_config, f)
