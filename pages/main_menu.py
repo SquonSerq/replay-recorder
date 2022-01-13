dc9792b626d4a7af5620639b64424e74bc4a63b0
Rettera <trof1m00@mail.ru>
2022-01-13 15:01:46 +0300

    feature: Create MainMenu layout

--- pages/main_menu.py
+++ pages/main_menu.py
@@ -1,48 +1,64 @@
-from tkinter import *
-from tkinter import filedialog, _setit
-from tkinter.ttk import *
+import tkinter as tk
+import tkinter.ttk as ttk
 from os import listdir
+from utils.colors import *
 
-class MainMenu(Frame):
+
+class MainMenu(ttk.Frame):
 	def __init__(self, parent, controller):
 
-		Frame.__init__(self, parent)
+		ttk.Frame.__init__(self, parent)
 		self.controller = controller
-		self.window_size = (600, 85)
+		self.window_size = (800, 600)
 
 		def choose_replay():
-			file = filedialog.askopenfile()
+			file = tk.filedialog.askopenfile()
 			if file:
-				replay.config(state='normal')
 				self.controller.config.replay_path = file.name
-				replay.delete(0, END)
-				replay.insert(END, file.name)
-				replay.config(state='disabled')
 		
 		def update_skin_selector(event):
 			if self.controller.config.is_dirs_valid():
 				skin_selector['menu'].delete(0, 'end')
 				for item in listdir(self.controller.config.danser_config['General']['OsuSkinsDir']):
-					skin_selector['menu'].add_command(label=item, command=_setit(self.controller.config.skin_name, item))
+					skin_selector['menu'].add_command(label=item, command=tk._setit(self.controller.config.skin_name, item))
 
-		# Interface to choose replay file
-		Label(self, text='Selected replay', width=20).grid(row=0, column=0)
-		replay = Entry(self, width=50, state='disabled')
-		replay.insert(END, self.controller.config.replay_path)
-		replay.grid(row=0, column=1)
-		Button(self, text='Choose replay', command=lambda: choose_replay(), width=20).grid(row=0, column=2)
 
 		# Interface to choose skin
-		Label(self, text='Selected skin', width=20).grid(row=1, column=0)
+		top_menu = tk.Frame(self, height=100)
+		top_menu.pack(side=tk.TOP, fill=tk.X)
+
+		top_menu_settings = tk.Frame(top_menu, bg=not_quite_black, height=20)
+		top_menu_settings.pack(side=tk.TOP, fill=tk.X)
+		top_menu_settings.pack_propagate(0)
+		tk.Button(top_menu_settings, text='Settings', command=lambda: controller.show_frame('Settings'), width=20, background=blueple, fg=white, borderwidth=0).pack(side=tk.RIGHT)
+
+		top_menu_skin = tk.Frame(top_menu, bg=dark_gray, height=80)
+		top_menu_skin.pack(side=tk.TOP, fill=tk.X)
+		top_menu_skin.pack_propagate(0)
+		tk.Label(top_menu_skin, text='Selected skin', width=20).pack(side=tk.LEFT, padx="25")
+
 		skin_list = ['default']
 		self.controller.config.skin_name.set(skin_list[0])
 		if self.controller.config.is_dirs_valid():
 			for item in listdir(self.controller.config.danser_config['General']['OsuSkinsDir']):
 				skin_list.append(item)
-		skin_selector = OptionMenu(self, self.controller.config.skin_name, *skin_list)
-		skin_selector.bind('<1>', update_skin_selector)
-		skin_selector.grid(row=1, column=1)
 
-		Button(self, text="Open videos folder", command=lambda: controller.open_videos_folder(), width=20).grid(row=2, column=0)
-		Button(self, text='Render video', command=lambda: controller.render_video(), width=20).grid(row=2, column=1)
-		Button(self, text='Settings', command=lambda: controller.show_frame('Settings'), width=20).grid(row=2, column=2)
+		skin_selector = ttk.OptionMenu(top_menu_skin, self.controller.config.skin_name, *skin_list)
+		skin_selector.bind('<1>', update_skin_selector)
+		skin_selector.pack(side=tk.LEFT)
+
+		# Frame for control buttons and render queue
+		middle_frame = tk.Frame(self, height=500)
+		middle_frame.pack(side=tk.TOP, fill=tk.X)
+
+		middle_frame_settings = tk.Frame(middle_frame, bg=not_quite_black, height=20)
+		middle_frame_settings.pack(side=tk.TOP, fill=tk.X)
+		middle_frame_settings.pack_propagate(0)
+		middle_frame_settings.columnconfigure((0, 1, 2), weight=1)
+		tk.Button(middle_frame_settings, text='Add replay', command=lambda: choose_replay(), width=20, background=blueple, fg=white, borderwidth=0).grid(row=0, column=0)
+		tk.Button(middle_frame_settings, text='Render video', command=lambda: controller.render_video(), width=20, background=blueple, fg=white, borderwidth=0).grid(row=0, column=1)
+		tk.Button(middle_frame_settings, text="Open videos folder", command=lambda: controller.open_videos_folder(), width=20, background=blueple, fg=white, borderwidth=0).grid(row=0, column=2)
+		
+		replays_frame = tk.Frame(middle_frame, bg=dark_gray, height=480)
+		replays_frame.pack(side=tk.TOP, fill=tk.X)
+		middle_frame_settings.pack_propagate(0)
---
+++ utils/colors.py
@@ -0,0 +1,4 @@
+not_quite_black = '#23272A'
+dark_gray = '#2C2F33'
+grayple = '#99AAB5'
+blueple = '#7289DA'
